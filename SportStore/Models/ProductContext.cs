376712270d401ad2988407a46f88b44c97ea3803
vlddryga2233 <vlddryga@gmail.com>
2020-09-12 13:33:36 +0300

    InitialCommit|Creating Database And Models

---
+++ SportStore/Models/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Models
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductContext(DbContextOptions options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ SportStore/Models/RepositoryProducts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Models
+{
+    public class RepositoryProducts : IProductRepository
+    {
+        private ProductContext context;
+        public RepositoryProducts(ProductContext productContext)
+        {
+            context = productContext;
+        }
+        public IQueryable<Product> Products => context.Products;
+    }
+}
